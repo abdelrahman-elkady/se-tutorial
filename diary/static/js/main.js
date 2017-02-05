9075a8a762cd789c009f9bd810a10b868436e985
kady <elkadyabdelrahman@gmail.com>
2017-02-05 19:12:22 +0200

    Clean up - II

--- diary/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$('.post-load-btn').on('click', function (event) {
-    $.ajax({
-        url: 'api/post',
-        success: function (post) {
-            $('.post-list-item-header').html(post.header);
-            $('.post-list-item-body').html(post.body);
-        }
-    });
-})
